b12f09e8809e6d1811241bc89f83365d48cacd01
GonzalezNacho <nacho.ldm63@gmail.com>
2022-08-02 11:49:21 -0300

    sacando la funcion de escribir en el nombre

--- js/script.js
+++ js/script.js
@@ -6,14 +6,3 @@ function desplazarBannerAbajo() {
         banner.className = "conteiner bannerDiv";
     }
 }
-
-function insertarTexto(span) {
-    span.className += "Desplazado";
-    span.desingMode='on';
-    span.textContent =" ";
-}
-
-function estadoInicial(span) {
-    span.className = "cursor";
-    span.textContent ="│";
-}
